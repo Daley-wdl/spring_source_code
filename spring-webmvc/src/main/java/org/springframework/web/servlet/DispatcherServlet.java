f20421d54298237b83e23f38f77b643365db1acb
wudelong <wudelong@wecash.net>
2020-04-23 11:36:39 +0800

    分析springMvc请求流程

--- spring-webmvc/src/main/java/org/springframework/web/servlet/DispatcherServlet.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/DispatcherServlet.java
@@ -53,6 +53,8 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 /**
+ * https://www.jianshu.com/p/2372548defdb : 分析springMvc请求流程
+ *
  * Central dispatcher for HTTP request handlers/controllers, e.g. for web UI controllers
  * or HTTP-based remote service exporters. Dispatches to registered handlers for processing
  * a web request, providing convenient mapping and exception handling facilities.
